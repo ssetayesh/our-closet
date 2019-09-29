8c1a32494d403f44fa11ea63349c76af4d74187f
Shirin Setayesh <shannonsetayesh@gmail.com>
2019-09-29 19:47:37 -0400

    style changes

--- screens/Profile.js
+++ screens/Profile.js
@@ -34,8 +34,6 @@ class Profile extends React.Component {
       aspect: [4, 3],
     });
 
-    console.log(result);
-
     if (!result.cancelled) {
       this.setState({ photos: result.uri });
     }
@@ -50,7 +48,9 @@ class Profile extends React.Component {
         <Image style={{ width: 200, height: 200, borderRadius: 200 / 2 }} source={{ uri: this.props.user.photoURL }} />
         <Text>{"\n"}</Text>
         <Text style={styles.profileTextContainer}>Let's recycle together! {"\n"}What clothes do you want to put in your closet?</Text>
+        <Text>{"\n"}Your current closet:{"\n"}</Text>
         <Button title={'Upload Clothing'} onPress={() => this.pickImage()} />
+        <Text>{"\n"}</Text>
         <Button onPress={() => this.props.dispatch(logout())} title={'Logout'} />
       </View >
     )
