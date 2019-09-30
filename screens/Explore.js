29b8995bf2e6a56abfd8dffe2012807011e37c09
Shirin Setayesh <shannonsetayesh@gmail.com>
2019-09-29 22:51:01 -0400

    swiping

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Text, View, Image } from 'react-native';
 import styles from '../styles';
 import { connect } from 'react-redux';
-import { login } from '../store/actions';
+import SwipeCards from 'react-native-swipe-cards';
 
 class Home extends React.Component {
-  state = {}
-
-  componentDidMount() {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      closets: [
+        { name: 'Laila', image: 'https://66.media.tumblr.com/17b171b8be8d46e84a917b9b3a04906b/tumblr_ogc9geYR6x1ub0xcto1_400.jpg' },
+        { name: 'Flora', image: '' }
+      ]
+    }
   }
 
+
   render() {
+    console.log('props in explore', this.props);
     return (
       <View style={styles.container}>
-        <Text>Explore Clothing! </Text>
+        <Text style={{ fontSize: 20, fontFamily: 'Georgia', fontWeight: 'bold' }}>{"\n"}{this.state.closets[0].name}'s closet!</Text>
+        <Image style={{ width: 200, height: 200 }} source={{ uri: this.state.closets[0].image }}></Image>
+        <SwipeCards closet={this.props.closets}></SwipeCards>
       </View>
     )
   }
